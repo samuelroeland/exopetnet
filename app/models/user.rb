b52b99709b7ece1b48069310edc39531ad0888a0
samuel van de ven <samuel.vandeven@gmail.com>
2022-11-21 16:03:37 +0000

    sign up forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :animals
   validates :username, presence: true
-  validates :user_type, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
