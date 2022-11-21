27941bebc85f589fce4bdb1c236a7dbe7fd5d429
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-21 19:47:10 +0000

    last changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path:'users'
   root to: "pages#home"
-
   resources :users do
     resources :animals, only: %i[new create edit update destroy]
   end
   resources :animals, only: %i[index show]
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
