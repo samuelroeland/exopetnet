5c822c027055a4461d0433d6e1f7ac9effc5e33e
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-22 15:34:05 +0000

    seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "purging animal images..."
+animals = Animal.all
+
+animals.each do |animal|
+  animal.image.purge
+end
+
+puts 'destroying all users and animals'
+Animal.destroy_all
+User.destroy_all
+
+puts 'creating 15 users'
+5.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: 'password',
+    username: Faker::Internet.username
+  )
+  puts "creating user id: #{user.id}"
+  3.times do
+    animal = Animal.new(
+      name: Faker::Name.name,
+      specie: Faker::Creature::Animal.name,
+      description: Faker::Quote.famous_last_words,
+      price: rand(3..15_000),
+      origin: ['north america', 'africa', 'europe', 'asia', 'south america', 'antartica', 'oceania'].sample,
+      stock: rand(0..25),
+      user_id: user.id
+    )
+    file = URI.open("app/assets/images/gerenuk.jpeg")
+    animal.image.attach(io: file, filename: 'gerenuk.jpeg', content_type: 'image/jpeg')
+    animal.save
+  end
+end
+puts '15 users with animal created'
