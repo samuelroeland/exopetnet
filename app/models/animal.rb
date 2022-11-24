047a5c1b5e5b540dc4b16022dd5fb7a93ee8a40e
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-24 12:06:54 +0000

    mapbox on index and without markers

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,10 +1,12 @@
 class Animal < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :name, presence: true
   validates :specie, presence: true
-  validates :description, presence:true
-  validates :price, presence:true
+  validates :description, presence: true
+  validates :price, presence: true
   validates :origin, presence: true
   has_many_attached :images
 end
